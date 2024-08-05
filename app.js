ef6ec9042a5ac3291132a9c85ac7f543377c517b
Ayush <150588656+Ayush-Jaiswal-03@users.noreply.github.com>
2024-08-06 05:26:14 +0530

    4

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://career-connect-18.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
